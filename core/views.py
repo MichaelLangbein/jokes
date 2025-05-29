e9706839967f0e84a5d95b0fe0f33270bbeea225
Michael Langbein <michael_langbein@hotmail.de>
2025-05-29 14:47:04 +0200

    working login screen

--- core/views.py
+++ core/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
-def startPage(req):
-    return render(req, 'baseLayout/startPage.html')
+def loginUser(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "Invalid username or password")
+            return render(request, 'loginPage.html')
+    else:
+        return render(request, 'loginPage.html')
+
+
+def logoutUser(request):
+    logout(request)
+    redirect('home')
+
+
+def homePage(req):
+    return render(req, 'homePage.html')
